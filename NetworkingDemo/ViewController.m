7ff4887efc967dfc45d7b822eb971151749925a1
David A Nichol <dan1@ad.rice.edu@staff-72-brown110-224.rice.edu>
2015-02-03 11:50:43 -0600

    Toss tiles

--- NetworkingDemo/ViewController.m
+++ NetworkingDemo/ViewController.m
@@ -398,7 +398,7 @@ int TILE_HEIGHT;
         dto.pending = 1;
         [self.boardCollectionView reloadData];
         
-        [self removeTile:tile];
+        [self removeTileFromCurrentSpot:tile];
         
         CGRect newFrame = CGRectMake(bvc.frame.origin.x + self.boardCollectionView.frame.origin.x, bvc.frame.origin.y+ self.boardCollectionView.frame.origin.y, tile.frame.size.width, tile.frame.size.height);
         tile.frame = newFrame;
@@ -411,8 +411,11 @@ int TILE_HEIGHT;
     }
     return NO;
 }
-
 -(void) removeTile:(TileViewCell *)tile {
+    [self removeTileFromCurrentSpot:tile];
+    [tile removeFromSuperview];
+}
+-(void) removeTileFromCurrentSpot:(TileViewCell *)tile {
     if (tile.isNotOnBoard) {
         CGRect rec = CGRectMake(tile.startPoint.x, tile.startPoint.y, tile.frame.size.width, tile.frame.size.height);
         currentPlayer.numberOfTiles--;
