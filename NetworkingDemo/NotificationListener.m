fe90e93bbdb855eb3b00f684bb023342f903f966
Kyle <kyle.bailey@ymail.com>
2015-02-15 12:35:22 -0600

    fixed line letting us start our own game for testing

--- NetworkingDemo/NotificationListener.m
+++ NetworkingDemo/NotificationListener.m
@@ -49,7 +49,7 @@
     
     if([chatEvent.message isEqualToString:@"joined"]){
         if([chatEvent.sender isEqualToString:[GameConstants getUserName]]){
-            [[LobbyViewController sharedViewController] playerJoinedLobby];
+            //[[LobbyViewController sharedViewController] startGame];
             return;
         }
         [[LobbyViewController sharedViewController] playerJoinedLobby];
