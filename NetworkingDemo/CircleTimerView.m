cacf108ddd69401bad96a04c9769986a9a7547b6
David A Nichol <dan1@ad.rice.edu@staff-72-brown110-224.rice.edu>
2015-02-14 22:54:25 -0600

    More circle/ui changes

--- NetworkingDemo/CircleTimerView.m
+++ NetworkingDemo/CircleTimerView.m
@@ -31,25 +31,31 @@ float endAngle = (3.5 * M_PI);
 {
     [super drawRect:rect];
     self.userInteractionEnabled = NO;
-    // Display our percentage as a string
     NSString* textContentSeconds = [NSString stringWithFormat:@"%d", self.seconds];
     NSString* textContentMilliseconds = [NSString stringWithFormat:@"%d", self.milliseconds/10];
     
     UIBezierPath* bezierPath = [UIBezierPath bezierPath];
     
-    // Create our arc, with the correct angles
     [bezierPath addArcWithCenter:CGPointMake(rect.size.width / 2, rect.size.height / 2)
+                          radius:120
+                      startAngle:startAngle
+                        endAngle:endAngle
+                       clockwise:YES];
+    bezierPath.lineWidth = 20;
+    [[UIColor whiteColor] setStroke];
+    [bezierPath stroke];
+    UIBezierPath* bezierPath2 = [UIBezierPath bezierPath];
+    
+    [bezierPath2 addArcWithCenter:CGPointMake(rect.size.width / 2, rect.size.height / 2)
                           radius:120
                       startAngle:startAngle
                         endAngle:(endAngle - startAngle) * (self.percent) + startAngle
                        clockwise:YES];
     
-    // Set the display for the path, and stroke it
-    bezierPath.lineWidth = 20;
+    bezierPath2.lineWidth = 20;
     [[UIColor blackColor] setStroke];
-    [bezierPath stroke];
+    [bezierPath2 stroke];
     
-    // Text Drawing
     if (self.seconds > 5) {
         CGRect textRect = CGRectMake((rect.size.width / 2.0) - 71/2.0, (rect.size.height / 2.0) - 45/2.0, 71, 45);
         [[UIColor blackColor] setFill];
